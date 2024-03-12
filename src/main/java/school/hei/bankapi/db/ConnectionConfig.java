305edb40c903d748641ac25baee22dc2c15f8974
Maharavo1 <hei.maharavo@gmail.com>
2024-03-12 10:36:16 +0300

    feat : Add database connection configuration

---
+++ src/main/java/school/hei/bankapi/db/ConnectionConfig.java
@@ -0,0 +1,24 @@
+package school.hei.bankapi.db;
+
+import school.hei.bankapi.exeption.DatabaseConnectionException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionConfig {
+    private static final String URL = System.getenv("PG_URL");
+    private static final String USERNAME = System.getenv("PG_USERNAME");
+    private static final String PASSWORD = System.getenv("PG_PASSWORD");
+  public static Connection getConnection(String URL , String USERNAME , String PASSWORD ){
+      try {
+          Class.forName("org.postgresql.Driver");
+          return DriverManager.getConnection(URL , USERNAME , PASSWORD);
+      } catch (SQLException | ClassNotFoundException e) {
+          throw new DatabaseConnectionException(e.getMessage());
+      }
+  }
+  public static Connection getConnection(){
+      return getConnection(URL ,USERNAME , PASSWORD);
+  }
+}
