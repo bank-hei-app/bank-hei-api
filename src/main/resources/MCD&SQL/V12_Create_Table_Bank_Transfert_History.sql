f3f2615a854873c1d893d03de0ea501739b722d8
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-03-15 09:26:07 +0300

    feat: add script to create table Bank_transfert_history

--- src/main/resources/MCD&SQL/V12_Create_Table_Bank_Transfert_History.sql
+++ src/main/resources/MCD&SQL/V12_Create_Table_Bank_Transfert_History.sql
@@ -1,14 +1,6 @@
+CREATE TABLE Bank_transfert_history(
+    bank_transfert_history int primary key ,
+    bank_transfert int references Bank_transfert(bank_transfert_id)
+);
 
 
-
-
-
-
-
-
-          CREATE TABLE bank_transfert_list(
-              bank_transfert_list_id int primary key ,
-              bank_transfert_id int references Bank_transfert(bank_transfert_id),
-              account_sender_id int references Account(account_id),
-              account_recipients_id int references Account(account_id)
-          );
