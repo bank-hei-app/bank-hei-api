06ccc9619724776cacd0d4d99b1bf006b7adedfa
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-03-15 09:13:19 +0300

    feat: add script to create table Balance_type

---
+++ src/main/resources/MCD&SQL/V6_Create_Table_Balance_Type.sql
@@ -0,0 +1,4 @@
+                CREATE TABLE Balance_type(
+                             balance_type_id int primary key ,
+                             balance_type_name varchar(200)
+                );
