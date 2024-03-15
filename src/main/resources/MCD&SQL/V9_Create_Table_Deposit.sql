b8eebdf70f584e42c6bf830e959cdd25bc900b94
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-03-15 09:18:20 +0300

    feat: add script to create table Deposit

---
+++ src/main/resources/MCD&SQL/V9_Create_Table_Deposit.sql
@@ -0,0 +1,8 @@
+                CREATE TABLE Deposit(
+                        deposit_id int primary key ,
+                        account_id int references Account(account_id),
+                        date_of_retrait TIMESTAMP,
+                        amount double precision,
+                        balance_type_id int references Balance_type(balance_type_id),
+                        balance_category_id int references Balance_category(balance_category_id)
+                );
