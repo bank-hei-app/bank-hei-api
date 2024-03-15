eb5989bdb3a86309a0d92665b4858e31e5977c00
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-03-15 09:14:49 +0300

    feat: add script to create table Balance_category

---
+++ src/main/resources/MCD&SQL/V7_Create_Table_Balance_Category.sql
@@ -0,0 +1,5 @@
+                    CREATE TABLE Balance_category(
+                                 balance_category_id int primary key ,
+                                 balance_category_name varchar(200),
+                                 description text
+                    );
