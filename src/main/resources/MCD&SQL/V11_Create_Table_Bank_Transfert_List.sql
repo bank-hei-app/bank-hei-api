cc30e61fb6fc9dc4d90214a592e193b4311e9e7e
Voahary Mampionona <hei.zoarisoa@gmail.com>
2024-03-15 09:23:33 +0300

    feat: add script to create table Bank_transfert_list

---
+++ src/main/resources/MCD&SQL/V11_Create_Table_Bank_Transfert_List.sql
@@ -0,0 +1,6 @@
+                    CREATE TABLE bank_transfert_list(
+                                    bank_transfert_list_id int primary key ,
+                                    bank_transfert_id int references Bank_transfert(bank_transfert_id),
+                                    account_sender_id int references Account(account_id),
+                                    account_recipients_id int references Account(account_id)
+                    );
--- src/main/resources/MCD&SQL/V9_Create_Table_Deposit.sql
+++ src/main/resources/MCD&SQL/V9_Create_Table_Deposit.sql
@@ -1,7 +1,7 @@
                 CREATE TABLE Deposit(
                         deposit_id int primary key ,
                         account_id int references Account(account_id),
-                        date_of_retrait TIMESTAMP,
+                        date_of_deposit TIMESTAMP,
                         amount double precision,
                         balance_type_id int references Balance_type(balance_type_id),
                         balance_category_id int references Balance_category(balance_category_id)
