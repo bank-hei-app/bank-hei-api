9d92f886a1ab458e1d427871d5c5f8fd7c36d156
Maharavo1 <hei.maharavo@gmail.com>
2024-03-19 09:41:51 +0300

    feat: Define DefaultModel class and update interface crudoperations

---
+++ src/main/java/school/hei/bankapi/model/DefaultModel.java
@@ -0,0 +1,9 @@
+package school.hei.bankapi.model;
+
+public class DefaultModel {
+    public static final String TABLE_NAME = "";
+    public static final String ID = "";
+
+    public DefaultModel() {
+    }
+}
--- src/main/java/school/hei/bankapi/repository/CrudOperations.java
+++ src/main/java/school/hei/bankapi/repository/CrudOperations.java
@@ -1,17 +1,17 @@
 package school.hei.bankapi.repository;
+import org.yaml.snakeyaml.events.Event;
+import school.hei.bankapi.model.DefaultModel;
+
 import java.util.List;
 
-public interface CrudOperations<T> {
+public interface CrudOperations<T extends DefaultModel, ID> {
 
     List<T> findAll();
+    List<T> saveAll(List<T> toSave);
 
-    T findById(int id);
 
-    void save(T toSave);
-
-    void saveAll(List<T> toSave);
-
-    void update(T toUpdate);
-
-    void delete(int id);
+    T save(T toSave);
+    T findById(ID id);
+    T delete(ID id);
+    T update(ID id);
 }
