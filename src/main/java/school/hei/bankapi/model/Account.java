68aa935ab67592a9c11cdad8929b5c41190c145a
Maharavo1 <hei.maharavo@gmail.com>
2024-03-19 17:49:33 +0300

    feat: update model Account

--- src/main/java/school/hei/bankapi/model/Account.java
+++ src/main/java/school/hei/bankapi/model/Account.java
@@ -1,27 +1,53 @@
 package school.hei.bankapi.model;
 
+import school.hei.bankapi.utils.annotations.Column;
+import school.hei.bankapi.utils.annotations.Table;
+
 import java.util.Date;
 
-public class Account {
+@Table(table_name = Account.tableName, id = Account.iD)
+public class Account extends DefaultModel {
+    @Column(name = Account.iD)
     private int accountId;
+    @Column(name = Account.clientName2)
     private String clientName;
+    @Column(name = Account.clientLastName2)
     private String clientLastName;
+    @Column(name = Account.dateOfBirth2)
     private Date dateOfBirth;
+    @Column(name = Account.netSalaryPerMonth2)
     private double netSalaryPerMonth;
+    @Column(name = Account.accountNumber2)
     private String accountNumber;
+    @Column(name = Account.bankId2)
     private int bankId;
+    @Column(name = Account.defaultSolde2)
     private double defaultSolde;
 
+
+
+
+    public static final String tableName = "account";
+    public static final String iD = "account_id";
+    public static final  String clientName2 = "client_name";
+    public static final  String clientLastName2 = "client_last_name";
+    public static final  String dateOfBirth2 = "date_of_birth";
+    public static final  String netSalaryPerMonth2 = "net_salary_per_month";
+    public static final  String accountNumber2 = "account_number";
+    public static final  String bankId2 = "bank_id";
+    public static final  String defaultSolde2 = "default_solde";
+
     public Account(int accountId, String clientName, String clientLastName, Date dateOfBirth, double netSalaryPerMonth,
-                   String accountNumber, double defaultSolde, int bankId) {
+                   String accountNumber,int bankId , double defaultSolde ) {
         this.accountId = accountId;
         this.clientName = clientName;
         this.clientLastName = clientLastName;
         this.dateOfBirth = dateOfBirth;
         this.netSalaryPerMonth = netSalaryPerMonth;
         this.accountNumber = accountNumber;
-        this.defaultSolde = defaultSolde;
         this.bankId = bankId;
+        this.defaultSolde = defaultSolde;
+
     }
 
     public int getAccountId() {
